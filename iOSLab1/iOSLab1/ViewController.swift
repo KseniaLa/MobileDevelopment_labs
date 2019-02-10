cdd93092ee8684b108f2ad8ec3ae1c8cf5dbecc3
KseniaLa <ksenia.lashch@gmail.com>
2019-02-10 16:42:11 +0300

    add project

---
+++ iOSLab1/iOSLab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLab1
+//
+//  Created by Admin on 10.02.2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
