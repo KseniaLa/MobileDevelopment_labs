8424db571b359111aa1b32f9c1efee1b367701a9
KseniaLa <ksla1998@mail.ru>
2019-02-10 19:14:15 +0300

    add email and password fields validation, alert on error

--- iOSLab1/iOSLab1/ViewController.swift
+++ iOSLab1/iOSLab1/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func signIn(_ sender: Any) {
+        let login = loginField.text
+        let password = passwordField.text
+        
+        if login!.count == 0 {
+            let alert = UIAlertController(title: "Invalid", message: "Username can't be empty", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Got it", style: .default, handler: {(action: UIAlertAction!) in alert.dismiss(animated: true, completion: nil)}))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        } else if password!.count == 0 {
+            let alert = UIAlertController(title: "Invalid", message: "Password can't be empty", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Got it", style: .default, handler: {(action: UIAlertAction!) in alert.dismiss(animated: true, completion: nil)}))
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        } else {
+            let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "UserListViewController")
+            self.present(viewController, animated: true, completion: nil)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
