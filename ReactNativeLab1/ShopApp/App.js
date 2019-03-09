091cfba5b5118ddfe95487c793903201b20025d7
KseniaLa <ksla1998@mail.ru>
2019-03-09 14:48:34 +0300

    move screen class files to separate folder

--- ReactNativeLab1/ShopApp/App.js
+++ ReactNativeLab1/ShopApp/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Grid from 'react-native-grid-component';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
-import HomeScreen from './HomeScreen';
-import DetailsScreen from './DetailsScreen';
-import CartScreen from './CartScreen'
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+import CartScreen from './screens/CartScreen'
 
 const MainNavigator = createStackNavigator({
   Home: HomeScreen,
